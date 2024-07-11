0fa01c3a974237eaae4e705b7fb7da38e5f5a960
yeghro <yeghro@proton.me>
2024-07-11 01:02:38 -0700

    added some bad words to the content filter

--- src/utils/content-filter.ts
+++ src/utils/content-filter.ts
@@ -2,7 +2,26 @@ import { Event } from "../@types/event";
 
 export function isValidContent(event: Event): boolean {
   // List of forbidden words
-  const forbiddenWords = ["#loli", "#lolita", "#NSFW"];
+  const forbiddenWords = [
+    "#loli",
+    "nigger",
+    "NIGGER",
+    "nigger!",
+    "NIGGER!",
+    "NIGGERS",
+    "niggers",
+    "niggers!",
+    "NIGGERS!",
+    "niggers!!",
+    "NIGGERS!!",
+    "#LOLI",
+    "#LOLITA",
+    "#lolita",
+    "#NSFW",
+    "#nsfw",
+    "#lewd",
+    "#LEWD",
+  ];
 
   // Extract content from the event, defaulting to an empty string if not present
   const content = event.content || "";
